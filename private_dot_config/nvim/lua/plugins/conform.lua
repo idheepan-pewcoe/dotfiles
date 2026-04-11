fce67cf801c5c71ef71606a1d76cc4625445da6e
dheepan <dheepan.iyyampillai@perkinelmer.com>
2026-04-11 09:45:45 -0400

    feat: Don't ask permission when claude was started with dangerous permissions, add black formatter to conform

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      python = { "ruff", "ruff_organize_imports" },
+      python = { "black", "ruff", "ruff_organize_imports" },
     },
   },
 }
